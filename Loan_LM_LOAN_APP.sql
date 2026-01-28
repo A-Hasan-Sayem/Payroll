8b4fe559dd6229adf6f9245f88364490b1d06dd8
Abu Hasan Sayem <abu.sayem@bracits.com>
2026-01-28 18:29:07 +0600

    added data type with script

--- Loan_LM_LOAN_APP.sql
+++ Loan_LM_LOAN_APP.sql
@@ -25,6 +25,7 @@ INSERT INTO LM_LOAN_APP (
     CANCEL_DATE,
     LOAN_ACCT_MERGE_ID,
     FILE_REF,
+    FILE_REF,
     SIGNED_AGREEMENT,
     PREVIOUS_APP_INFO,
     MERGE_WITH_EXISTING_LOAN_ACCT,
@@ -32,7 +33,8 @@ INSERT INTO LM_LOAN_APP (
     LOAN_CATEGORY,
     CHILD_NAME,
     INSTITUTE_TYPE,
-    OVERRIDE_FLAG
+    OVERRIDE_FLAG,
+    COMMENTS
 )
 with loan_app as (select employeeid, applicationno,loanno,loanid, applieddate, appliedamount, remarks, applicationstatus, noofinstallment, installmentamount
 from "Airbyte".loan_loanapplication
@@ -42,40 +44,42 @@ SELECT lam.employeeid,applicationid, lam.slno AS loanno,lam.loanid,lam.appliedda
 FROM "Airbyte".loan_loanapplication_manual lam left join "Airbyte".loan_loanapplication la on lam.slno = la.loanno
 WHERE applicationapprovedyn = 1 and la.loanno is null and lam.slno is not null)
 SELECT
-    gen_random_uuid() AS ID,
-    0 AS VERSION,
-    1 AS CREATED_BY,
-    NOW() AS CREATED_DATE,
-    1 AS LAST_MODIFIED_BY,
-    NOW() AS LAST_MODIFIED_DATE,
-    NULL AS LOAN_APP_NO,
-    la.APPLIEDDATE AS LOAN_APP_DATE,
-    ep.company_id AS COMPANY_ID,
-    d_le.id AS LOAN_ACCT_ID,
-    'Hardcode Master Data' AS EMPLOYEE_FUND_PROFILE_ID, -- MASTER DATA pending
-    ep.id AS EMPL_PROFILE_ID,
-    la.loanid AS LOAN_TYPE_ID, --make sure where will it come from la or le
-    la.remarks AS LOAN_PURPOSE, --make sure where will it come from la or le
-    la.APPLIEDAMOUNT AS AMOUNT, --make sure where will it come from la or le
-    la.NOOFINSTALLMENT AS NO_OF_INSTALLMENT, --make sure where will it come from la or le
-    la.INSTALLMENTAMOUNT AS AMOUNT_INSTALLMENT, --make sure where will it come from la or le
-    NULL AS DOC_IMAGE,
-    NULL AS DOC_NAME,
-    '40' AS PROCESS_STATE, --APPROVED(40)
-    NULL AS APPROVER_USERNAME,
-    sal.id AS SPL_ALLOW_LEVEL_ID, -- only if applicable
-    NULL AS APPROVER_DATE,
-    NULL AS CANCEL_DATE,
-    NULL AS LOAN_ACCT_MERGE_ID,
-    NULL AS FILE_REF,
-    NULL AS SIGNED_AGREEMENT,
-    NULL AS PREVIOUS_APP_INFO,
-    FALSE AS MERGE_WITH_EXISTING_LOAN_ACCT,
-    NULL AS PURPOSE_ID,
-    NULL AS LOAN_CATEGORY,
-    NULL AS CHILD_NAME,
-    NULL AS INSTITUTE_TYPE,
-    NULL AS OVERRIDE_FLAG
+    gen_random_uuid()::uuid AS ID,
+    1::integer AS VERSION,
+    '1'::varchar AS CREATED_BY,
+    NOW()::timestamp with time zone AS CREATED_DATE,
+    '1'::varchar AS LAST_MODIFIED_BY,
+    NOW()::timestamp with time zone AS LAST_MODIFIED_DATE,
+    NULL::varchar AS LOAN_APP_NO,
+    la.APPLIEDDATE::date AS LOAN_APP_DATE,
+    ep.company_id::uuid AS COMPANY_ID,
+    d_le.id::uuid AS LOAN_ACCT_ID,
+    'Hardcode Master Data'::uuid AS EMPLOYEE_FUND_PROFILE_ID, -- MASTER DATA pending
+    ep.id::uuid AS EMPL_PROFILE_ID,
+    la.loanid::uuid AS LOAN_TYPE_ID, --make sure where will it come from la or le
+    la.remarks::text AS LOAN_PURPOSE, --make sure where will it come from la or le
+    la.APPLIEDAMOUNT::double precision AS AMOUNT, --make sure where will it come from la or le
+    la.NOOFINSTALLMENT::integer AS NO_OF_INSTALLMENT, --make sure where will it come from la or le
+    la.INSTALLMENTAMOUNT::double precision AS AMOUNT_INSTALLMENT, --make sure where will it come from la or le
+    NULL::bytea AS DOC_IMAGE,
+    NULL::varchar AS DOC_NAME,
+    40::integer AS PROCESS_STATE, --APPROVED(40)
+    NULL::varchar AS APPROVER_USERNAME,
+    sal.id::uuid AS SPL_ALLOW_LEVEL_ID, -- only if applicable
+    NULL::timestamp with time zone AS APPROVER_DATE,
+    NULL::timestamp with time zone AS CANCEL_DATE,
+    NULL::uuid AS LOAN_ACCT_MERGE_ID,
+    NULL::varchar AS FILE_REF,
+    NULL::varchar AS FILE_REF,
+    NULL::varchar AS SIGNED_AGREEMENT,
+    NULL::text AS PREVIOUS_APP_INFO,
+    FALSE::boolean AS MERGE_WITH_EXISTING_LOAN_ACCT,
+    NULL::uuid AS PURPOSE_ID,
+    NULL::integer AS LOAN_CATEGORY,
+    NULL::varchar AS CHILD_NAME,
+    NULL::integer AS INSTITUTE_TYPE,
+    NULL::boolean AS OVERRIDE_FLAG,
+    NULL::varchar as COMMENTS
 FROM  "Airbyte".loan_loanemployee le left join  loan_app la on le.loanno = la.loanno
     JOIN payroll_leave_attend_data.employee_records_all ie ON ie.pin = la.employeeid
     JOIN public.mdg_empl_profile ep ON ep.employee_code = ie.pin
--- Loan_LM_LOAN_APP_DOC.sql
+++ Loan_LM_LOAN_APP_DOC.sql
@@ -11,15 +11,15 @@ INSERT INTO LM_LOAN_APP_DOC (
     FILE_REF
 )
 SELECT
-    gen_random_uuid() AS ID,
-    1 AS VERSION,
-    1 AS CREATED_BY,
-    NOW() AS CREATED_DATE,
-    1 AS LAST_MODIFIED_BY,
-    NOW() AS LAST_MODIFIED_DATE,
-    la.attachment AS DESCRIPTION,
-    NULL AS DOC_IMAGE,     -- YET TO BE MAPPED
-    d_la.ID AS LOAN_APP_ID,
-    NULL AS FILE_REF       -- YET TO BE MAPPED
+    gen_random_uuid()::uuid AS ID,
+    1::integer AS VERSION,
+    '1'::varchar AS CREATED_BY,
+    NOW()::timestamp with time zone AS CREATED_DATE,
+    '1'::varchar AS LAST_MODIFIED_BY,
+    NOW()::timestamp with time zone AS LAST_MODIFIED_DATE,
+    la.attachment::varchar AS DESCRIPTION,
+    NULL::bytea AS DOC_IMAGE,     -- YET TO BE MAPPED
+    d_la.ID::uuid AS LOAN_APP_ID,
+    NULL::varchar AS FILE_REF       -- YET TO BE MAPPED
 FROM "Airbyte".loan_loanapplication la join public.LM_LOAN_ACCT d_le on la.loanno = d_le.loan_no
     JOIN public.LM_LOAN_APP d_la on d_la.LOAN_ACCT_ID = d_le.id;
